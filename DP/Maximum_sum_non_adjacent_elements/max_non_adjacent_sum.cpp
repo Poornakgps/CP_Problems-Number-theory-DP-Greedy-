31038920ca68bbb2cfdfa9856f835da88c87d8af
Poorna <king@gmail.com>
2022-12-07 11:02:28 +0530

    added max_non adjacent sum Algorithm

---
+++ DP/Maximum_sum_non_adjacent_elements/max_non_adjacent_sum.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+#define ll long long
+#define MOD 1000000007
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+
+using namespace std;
+
+class max_sum_non_adjacent_elements
+{
+    public:
+
+        ll testcases,length_of_array;
+
+        void print_answer();
+
+        ll Algorithm()
+        {
+            ll array[length_of_array];
+            for (int i=0; i<length_of_array; i++)
+            {
+                cin>>array[i];
+            }
+            ll inc=array[0];  //including first element
+            ll exc=0;         //excluding first element
+
+            for(int i=1; i<length_of_array; i++)
+            {
+                ll temp1=inc;
+                ll temp2=exc;
+                inc=temp2+array[i];
+                exc=max(temp1,temp2);
+            }   
+            return max(inc,exc);
+        }
+};
+//accessing public class function from outside the class
+void max_sum_non_adjacent_elements::print_answer()
+{
+    cin>>length_of_array;
+    cout<<Algorithm()<<endl;
+}
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        max_sum_non_adjacent_elements object1;
+        cin>>object1.testcases;
+
+        object1.print_answer();
+
+    }
+}
